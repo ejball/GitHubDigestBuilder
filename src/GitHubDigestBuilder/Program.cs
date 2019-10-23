0b1c040584250ae45f88be4fd95a53a60a6c1472
Ed Ball <ed.ball@faithlife.com>
2019-10-22 20:54:48 -0700

    Catch exceptions.

--- src/GitHubDigestBuilder/Program.cs
+++ src/GitHubDigestBuilder/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace GitHubDigestBuilder
+﻿using System;
+using System.Collections.Generic;
+
+namespace GitHubDigestBuilder
 {
-	public sealed class Program
+	public static class Program
 	{
+		public static void Run(IReadOnlyList<string> args)
+		{
+			if (args.Count != 0)
+				throw new ApplicationException("No arguments supported yet.");
+		}
+
 		public static int Main(string[] args)
 		{
-			return args.Length;
+			try
+			{
+				Run(args);
+				return 0;
+			}
+			catch (ApplicationException exception)
+			{
+				Console.Error.WriteLine(exception.Message);
+				return 1;
+			}
+			catch (Exception exception)
+			{
+				Console.Error.WriteLine(exception);
+				return 1;
+			}
 		}
 	}
 }
