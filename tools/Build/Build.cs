ca099b9a3c4985c544133fdd6f4a9bcb1e97f748
Ed Ball <ed.ball@faithlife.com>
2020-01-18 21:08:57 -0800

    Don't publish C# docs.

--- tools/Build/Build.cs
+++ tools/Build/Build.cs
@@ -9,12 +9,6 @@ internal static class Build
 			new DotNetBuildSettings
 			{
 				NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY"),
-				DocsSettings = new DotNetDocsSettings
-				{
-					GitLogin = new GitLoginInfo("ejball", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? ""),
-					GitAuthor = new GitAuthorInfo("ejball", "ejball@gmail.com"),
-					SourceCodeUrl = "https://github.com/ejball/GitHubDigestBuilder/tree/master/src",
-				},
 			});
 	});
 }
