5f0b4d16f8cd7d85b43fe8e69913005dbde08102
Ed Ball <ed.ball@faithlife.com>
2021-03-21 20:53:04 -0700

    Don't try to generate docs.

--- tools/Build/Build.cs
+++ tools/Build/Build.cs
@@ -9,12 +9,6 @@ return BuildRunner.Execute(args, build =>
 		new DotNetBuildSettings
 		{
 			NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY"),
-			DocsSettings = new DotNetDocsSettings
-			{
-				GitLogin = gitLogin,
-				GitAuthor = new GitAuthorInfo("ejball", "ejball@gmail.com"),
-				SourceCodeUrl = "https://github.com/ejball/RepoName/tree/master/src",
-			},
 			PackageSettings = new DotNetPackageSettings
 			{
 				GitLogin = gitLogin,
